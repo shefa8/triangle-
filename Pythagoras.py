9ec359c0ce69bc90bfa5c5a4514548cd6889d1d9
shefa8 <shefa.agammdli@gmail.com>
2023-04-19 17:37:31 +0400

    cmmt

---
+++ Pythagoras.py
@@ -0,0 +1,16 @@
+def triangle(a,b,c):
+     if (a**2+b**2)==c**2:
+          result="yu re correct:)"
+     else:
+          result="unfortunately:("
+     return result
+
+print(triangle(2,4,5))
+       
+
+   
+
+
+
+
+    
